7b454cf3f55805d2c2e7b3233481d5c80a0dd0e2
Sema <jazz-dev@jazz-soft.net>
2024-07-26 23:41:12 -0400

    Reworking parser

--- jazz-abc.js
+++ jazz-abc.js
@@ -1,4 +1,11 @@
-function Parser(s, opt) {
+function Parser(s) {
+  this.txt = s;
+  this.data = [];
+  var lines = s.split(/\r?\n|\r/);
+  for (var i = 0; i < lines.length; i++) this.data.push(new Tokens(lines[i], i))
+}
+
+function Tokens(s) {
 }
 
 module.exports = {
