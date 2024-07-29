e223c991ed1fdb9efaee1ec2d85952c45b40b580
Sema <jazz-dev@jazz-soft.net>
2024-07-28 23:22:24 -0400

    Tokenizer - in progress

--- jazz-abc.js
+++ jazz-abc.js
@@ -2,10 +2,23 @@ function Parser(s) {
   this.txt = s;
   this.data = [];
   var lines = s.split(/\r?\n|\r/);
-  for (var i = 0; i < lines.length; i++) this.data.push(new Tokens(lines[i], i))
+  for (var i = 0; i < lines.length; i++) this.data.push(new Tokens(lines[i], i, 0));
 }
 
-function Tokens(s) {
+function Tokens(s, r, c, t) {
+  this.txt = s;
+  if (s[1] == ':') {
+    if (_isLetter(s[0])) t = s[0];
+  }
+  return { r: r, c: c, x: s };
+}
+var _A = 'A'.charCodeAt(0);
+var _Z = 'Z'.charCodeAt(0);
+var _a = 'a'.charCodeAt(0);
+var _z = 'z'.charCodeAt(0);
+function _isLetter(c) {
+  c = c.charCodeAt(0);
+  return c >= _A && c <= _Z || c >= _a && c <= _z;
 }
 
 module.exports = {
