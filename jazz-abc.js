1f4626792ffa8749db1d694c06b9d1b411fb3c2e
Sema <jazz-dev@jazz-soft.net>
2024-08-17 18:59:20 -0400

    Pseudocomments

--- jazz-abc.js
+++ jazz-abc.js
@@ -1,40 +1,53 @@
 function Parser(s) {
   this.txt = s;
-  this.data = [];
+  this.tokens = [];
   var lines = s.split(/\r?\n|\r/);
-  for (var i = 0; i < lines.length; i++) this.data.push(new Tokens(lines[i], i, 0));
+  for (var i = 0; i < lines.length; i++) this.tokens.push(new Tokens(lines[i], i, 0));
 }
 
 function Tokens(s, l, c, t) {
-  var i, x;
+  var i, a, x;
   if (s[1] == ':' && _isField(s[0])) {
     x = s.substring(0, 2);
     if (s[0] != '+') t = s[0];
-    return [{ l: l, c: c, t: x, x: x }].concat(_plain(s.substring(2), l, c + 2));
+    a = _chop(s.substring(2), l, c + 2);
+    return [{ l: l, c: c, t: x, x: x }].concat(a);
   }
   for (i = 0; i < s.length; i++) if (!_isSpace(s[i])) break;
   c += i;
-  if (s[i] == '%') return _percent(s.substring(i), l, c); //[{ l: l, c: c, t: '%', x: s.trim() }];
-  return [{ l: l, c: c, x: s.trim() }];
+  if (s[i] == '%') return _percent(s.substring(i), l, c);
+  x = s.trim();
+  return x == '' ? [] : [{ l: l, c: c, x: s.trim() }];
 }
-function _plain(s, l, c) {
+function _chop(s, l, c) {
   var a = [];
   var i, k, q, x;
   for (i = 0; i < s.length; i++) if (!_isSpace(s[i])) break;
   if (i == s.length) return a;
   for (k = i; k < s.length; k++) {
     if (s[k] == '\\') q = !q;
-    if (s[k] == '%' && !q) {
+    else if (s[k] == '%' && !q) {
       x = s.substring(i, k).trim();
       if (x) a.push({ l: l, c: c + i, x: x });
       a.push({ l: l, c: c + k, t: '%', x: s.substring(k).trim() });
       return a;
     }
+    else q = false;
   }
   a.push({ l: l, c: c + i, x: s.substring(i).trim() });
   return a;
 }
 function _percent(s, l, c) {
+  var i;
+  if (!c) {
+    if (s[1] == '%') {
+      for (i = 2; i < s.length; i++) if (!_isLetter(s[i])) break;
+      return [{ l: l, c: c, t: '%%', x: s.substring(0, i) }].concat(_chop(s.substring(i), l, c + i));
+    }
+    else if (s.startsWith('%abc') && !_isLetter(s[4])) {
+      return [{ l: l, c: c, t: '%:', x: '%abc' }].concat(_chop(s.substring(4), l, c + 4));
+    }
+  }
   return [{ l: l, c: c, t: '%', x: s.trim() }];
 }
 var _A = 'A'.charCodeAt(0);
@@ -42,7 +55,7 @@ var _Z = 'Z'.charCodeAt(0);
 var _a = 'a'.charCodeAt(0);
 var _z = 'z'.charCodeAt(0);
 function _isLetter(c) {
-  c = c.charCodeAt(0);
+  c = c ? c.charCodeAt(0) : 0;
   return c >= _A && c <= _Z || c >= _a && c <= _z;
 }
 function _isField(c) { return c == '+' || _isLetter(c); }
--- test/mocha.js
+++ test/mocha.js
@@ -18,18 +18,29 @@ function equal(a, b) {
 
 describe('tokenize', function() {
   it('comment', function() {
-    var P = new Parser('N: \nN:ab \nN: ac \\% c\nN: ac \\\\% c \nabc\nB: %');
+    var P = new Parser('N: \nN:ab \nN: ac \\% c\nN: ac \\\\% c \nabc\nB: %\nB:\\% \\%%\n');
     //console.log(P.data);
     equal(P.data, [
       [ { l: 0, c: 0, t: 'N:', x: 'N:' } ],
       [ { l: 1, c: 0, t: 'N:', x: 'N:' }, { l: 1, c: 2, x: 'ab' } ],
       [ { l: 2, c: 0, t: 'N:', x: 'N:' }, { l: 2, c: 3, x: 'ac \\% c' } ],
-      [ { l: 3, c: 0, t: 'N:', x: 'N:' },
-        { l: 3, c: 3, x: 'ac \\\\' },
-        { l: 3, c: 8, t: '%', x: '% c' }
-      ],
+      [ { l: 3, c: 0, t: 'N:', x: 'N:' }, { l: 3, c: 3, x: 'ac \\\\' }, { l: 3, c: 8, t: '%', x: '% c' } ],
       [ { l: 4, c: 0, x: 'abc' } ],
-      [ { l: 5, c: 0, t: 'B:', x: 'B:' }, { l: 5, c: 3, t: '%', x: '%' } ]
+      [ { l: 5, c: 0, t: 'B:', x: 'B:' }, { l: 5, c: 3, t: '%', x: '%' } ],
+      [ { l: 6, c: 0, t: 'B:', x: 'B:' }, { l: 6, c: 2, x: '\\% \\%' }, { l: 6, c: 7, t: '%', x: '%' } ],
+      [ ]
+    ]);
+  });
+  it('pseudocomment', function() {
+    var P = new Parser('%abc\n %abc\n%abc-2.1\n%abc %abc\n%abc-2.1%abc\n%%endtext %');
+    //console.log(P.data);
+    equal(P.data, [
+      [ { l: 0, c: 0, t: '%:', x: '%abc' } ],
+      [ { l: 1, c: 1, t: '%', x: '%abc' } ],
+      [ { l: 2, c: 0, t: '%:', x: '%abc' }, { l: 2, c: 4, x: '-2.1' } ],
+      [ { l: 3, c: 0, t: '%:', x: '%abc' }, { l: 3, c: 5, t: '%', x: '%abc' } ],
+      [ { l: 4, c: 0, t: '%:', x: '%abc' }, { l: 4, c: 4, x: '-2.1' }, { l: 4, c: 8, t: '%', x: '%abc' } ],
+      [ { l: 5, c: 0, t: '%%', x: '%%endtext' }, { l: 5, c: 10, t: '%', x: '%' } ]
     ]);
   });
 });
