dad2a7398ac9f5b128bf66ea054b6f5cc54fec22
Sema <jazz-dev@jazz-soft.net>
2024-08-30 22:15:06 -0400

    Fixed test

--- test/mocha.js
+++ test/mocha.js
@@ -19,13 +19,13 @@ function equal(a, b) {
 describe('tokenize', function() {
   it('comment', function() {
     var P = new Parser('N: \nN:ab \nN: ac \\% c\nN: ac \\\\% c \nabc\nB: %\nB:\\% \\%%\n');
-    //console.log(P.data);
+    //console.log(P.tokens);
     equal(P.tokens, [
       [ { l: 0, c: 0, t: 'N:', x: 'N:' } ],
       [ { l: 1, c: 0, t: 'N:', x: 'N:' }, { l: 1, c: 2, x: 'ab' } ],
       [ { l: 2, c: 0, t: 'N:', x: 'N:' }, { l: 2, c: 3, x: 'ac \\% c' } ],
       [ { l: 3, c: 0, t: 'N:', x: 'N:' }, { l: 3, c: 3, x: 'ac \\\\' }, { l: 3, c: 8, t: '%', x: '% c' } ],
-      [ { l: 4, c: 0, x: 'abc' } ],
+      [ { l: 4, c: 0, t: '??', x: 'abc' } ],
       [ { l: 5, c: 0, t: 'B:', x: 'B:' }, { l: 5, c: 3, t: '%', x: '%' } ],
       [ { l: 6, c: 0, t: 'B:', x: 'B:' }, { l: 6, c: 2, x: '\\% \\%' }, { l: 6, c: 7, t: '%', x: '%' } ],
       [ ]
